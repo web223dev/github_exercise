1c262cc88399f90f28cf32adc134e327864c9952
web223dev <ruslanus866@outlook.com>
2021-01-28 23:06:16 +0800

    added new files

---
+++ App.js
@@ -0,0 +1,12 @@
+import ImageGridItem from "./ImageGridItem";
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+          <ImageGridItem />
+      </div>
+  );
+}
+
+export default App;
